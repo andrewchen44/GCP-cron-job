57c13b736fe72b6bd0eaa2749b9448e0da0ac2b7
Andrew Chen <andrew.chen@Andrews-MacBook-Pro.local>
2018-10-02 10:17:53 -0700

    Modify cron job

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import webapp2
 import urllib2
-class DayCronPage(webapp2.RequestHandler):
+class HourCronPage(webapp2.RequestHandler):
     def get(self):
-        request = urllib2.Request('<GCF URL>', headers={"cronrequest" : "true"})
+        request = urllib2.Request('https://us-central1-clover-page-spee-1535578543408.cloudfunctions.net/google-speed-api-tester', headers={"cronrequest" : "true"})
         contents = urllib2.urlopen(request).read()
+
 app = webapp2.WSGIApplication([
-    ('/day', DayCronPage),
+    ('/hour', HourCronPage),
     ], debug=True)
