7856559897bd842d28243c9994f722c5d880aef5
Andrew Chen <andrew.chen@Andrews-MacBook-Pro.local>
2018-10-02 10:43:10 -0700

    Update cronjob to daily

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import webapp2
 import urllib2
-class HourCronPage(webapp2.RequestHandler):
+class DailyCronPage(webapp2.RequestHandler):
     def get(self):
         request = urllib2.Request('https://us-central1-clover-page-spee-1535578543408.cloudfunctions.net/google-speed-api-tester', headers={"cronrequest" : "true"})
         contents = urllib2.urlopen(request).read()
 
 app = webapp2.WSGIApplication([
-    ('/hour', HourCronPage),
+    ('/day', DailyrCronPage),
     ], debug=True)
